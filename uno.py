7fe3768237cd542320d4c81e6f1e605e51144e1a
SuiJin Foong <foongsj@gmail.com>
2022-12-13 15:41:47 +0800

    Using os.system to move files to archive

--- uno.py
+++ uno.py
@@ -4,6 +4,7 @@ import os
 import pprint
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+# from watchdog.tricks import ShellCommandTrick
 
 class Handler(FileSystemEventHandler):
   @staticmethod
@@ -14,10 +15,14 @@ class Handler(FileSystemEventHandler):
     if event.event_type == 'created':
       fn = event.src_path.split('/')[-1]
       print(f"New file created: {fn}")
-      print(f'First 4 characters are: {fn[:4]}')
+      fnmask = fn[:4]
+      print(f'First 4 characters are: {fnmask}')
+      os.system(f"mv `ls -t {fnmask}* | awk 'NR>1'` archive")
+
 
 observer = Observer()
 event_handler = Handler()
+# event_handler = ShellCommandTrick()
 observer.schedule(event_handler, '.')
 observer.start()
 try:
