ee76e9cbcd1c46c12fd9d742e0c97846e2ab3c71
SuiJin Foong <foongsj@gmail.com>
2022-12-13 15:13:22 +0800

    Using watchdog

--- uno.py
+++ uno.py
@@ -2,16 +2,29 @@
 
 import os
 import pprint
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-fns = {}
+class Handler(FileSystemEventHandler):
+  @staticmethod
+  def on_created(event):
+    if event.is_directory:
+      return None
 
-with os.scandir() as it:
-  for entry in it:
-    if not entry.name.startswith('.') and entry.is_file():
-      stat = entry.stat()
-      print(entry.name[:4], entry.name, stat.st_mtime)
-      try:
-        fns[entry.name[:4]][entry.name] = stat.st_mtime
-      except KeyError:
-        fns[entry.name[:4]] = {entry.name: stat.st_mtime}
-pprint.pprint(fns)
+    if event.event_type == 'created':
+      fn = event.src_path.split('/')[-1]
+      print(f"New file created: {fn}")
+      print(f'First 4 characters are: {fn[:4]}')
+
+observer = Observer()
+event_handler = Handler()
+observer.schedule(event_handler, '.')
+observer.start()
+try:
+  while observer.is_alive():
+    observer.join(60) # Sit and wait for events
+finally:
+  observer.stop()
+  observer.join()
+
+
